07d348eb71f18cc1b1eca44637bfa52895d8bae5
huzailhassan <61159990+huzailhassan@users.noreply.github.com>
2021-03-03 12:29:26 -0800

    Added the convertopostfix, still need to fix

--- LinkedStack.java
+++ LinkedStack.java
@@ -1,70 +1,99 @@
+import java.util.*;
+
 public class LinkedStack<T> implements StackInterface<T>
 {
-    private Node topNode;
+    private Node<T> topNode;
 
-    public LinkedStack()
-    {
+    public LinkedStack() {
         topNode = null;
     }
 
-    public void push(T newEntry)
-    {
-        Node newNode = new Node(newEntry, topNode);
+    public void push(T newEntry) {
+        Node<T> newNode = new Node<T>(newEntry, topNode);
         topNode = newNode;
         //topNode = new Node(newEntry, topNode); // Alternate code
     } // end push
 
-    public T pop()
-    {
+    public T pop() {
        T top = peek();  // Might throw EmptyStackException
        // Assertion: topNode != null
-       topNode = topNode.getNextNode();
+       topNode = topNode.getNext();
     
        return top;
     } // end pop
 
-    public T peek()
-    {
+    public T peek() {
        if (isEmpty())
           throw new EmptyStackException();
        else
           return topNode.getData();
     } // end peek
     
-    public boolean isEmpty()
-    {
+    public boolean isEmpty() {
         return topNode == null;
     } // end isEmpty
 
-    public void clear()
-    {
+    public void clear() {
         topNode = null;
     }
 
-    private class Node
-    {
-        private T data; // entry in stack
-        private Node next; //link to next node
+    public ResizableArrayStack<T> operatorStack;
+    public String postFix;
 
-        private T getData()
-    {
-    return data;
-    } // end getData
+    public String nextCharacter;
 
-    private void setData(T newData)
-    {
-        data = newData;
-    } // end setData
+    char topOperator;
 
-    private Node getNextNode()
-    {
-        return next;
-    } // end getNextNode
-
-    private void setNextNode(Node nextNode){
-        next = nextNode;
-    } // end setNextNode
-        
+    static int precedence(char c){
+        switch (c){
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+            case '^':
+                return 3;
+        }
+        return -1;
     }
+
+    public String convertToPostfix() {
+        while (!) {
+            switch (nextCharacter = ) {
+                case variable:
+                    break;
+                case '^':
+                    operatorStack.push(nextCharacter);
+                    break;
+                case '+' : case '-' : case '*' : case '/' :
+                    while (operatorStack.isEmpty()==false && precedence(operatorStack.peek())>=precedence(c)) {
+                        operatorStack.pop();
+                    }
+                    operatorStack.push(nextCharacter);
+                    break;
+                case '(':
+                    operatorStack.push(nextCharacter);
+                    break;
+                case ')':
+                    topOperator = operatorStack.pop();
+                    while(topOperator!='(') {
+                        postfix += x;
+                        topOperator = operatorStack.pop();
+                    }
+                    break;
+                default: 
+                    break;
+        
+        }
+        while (!operatorStack.isEmpty()) {
+            topOperator = operatorStack.pop();
+
+        }
+        return postfix;
+    }
+
+   return null;
 }
 
+}
