39f83f06c54aa87065cb83b9701322225924173a
Amar Gandhi <amargandhi10@gmail.com>
2021-03-05 23:22:40 -0800

    Add test to convertToPostfix test in LinkedStackTest. Move this to other class in future.

--- LinkedStackTest.java
+++ LinkedStackTest.java
@@ -1,1 +1,17 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+public class LinkedStackTest {
+
+    static final String GIVEN_INFIX_EXPRESSION = "a * b / (c - a) + d * e";
+    static final String EXPECTED_POSTFIX = "ab*ca-/de*+";
+    
+    public static void main(String[] args) {
+    
+    }
+    
+    @Test
+    public void convertToPostfix_Given() {
+        assertEquals(EXPECTED_POSTFIX, Convertor.convertToPostfix(GIVEN_INFIX_EXPRESSION.toCharArray()));
+    }
+}
