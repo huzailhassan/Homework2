975d9874102a88e0ee4848213c610b78a277cbc9
Amar Gandhi <amargandhi10@gmail.com>
2021-03-06 04:00:30 -0800

    Make test for leading whitespace

--- LinkedStackTest.java
+++ LinkedStackTest.java
@@ -4,9 +4,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class LinkedStackTest {
 
     static final String GIVEN_INFIX_EXPRESSION = "a * b / (c - a) + d * e";
-    static final String EXPECTED_POSTFIX = "ab*ca-/de*+";
+    static final String EXPECTED_GIVEN = "ab*ca-/de*+";
     
     static final String INFIX_TRAILING = GIVEN_INFIX_EXPRESSION + "\t\r\n  \t\r\n     \t\r\n";
+    
+    static final String INFIX_LEADING = "\t\r\n  \t\r\n     \t\r\n" + GIVEN_INFIX_EXPRESSION;
     
     public static void main(String[] args) {
     
@@ -14,12 +16,17 @@ public class LinkedStackTest {
     
     @Test
     public void convertToPostfix_Given() {
-        assertEquals(EXPECTED_POSTFIX, Converter.convertToPostfix(GIVEN_INFIX_EXPRESSION.toCharArray()));
+        assertEquals(EXPECTED_GIVEN, Converter.convertToPostfix(GIVEN_INFIX_EXPRESSION.toCharArray()));
+    }
     
     @Test
     public void convertToPostfix_TrailingWhitespace() {
         assertEquals(EXPECTED_GIVEN, Converter.convertToPostfix(INFIX_TRAILING.toCharArray()));
     }
     
+    @Test
+    public void convertToPostfix_LeadingWhitespace() {
+        assertEquals(EXPECTED_GIVEN, Converter.convertToPostfix(INFIX_LEADING.toCharArray()));
     }
+    
 }
