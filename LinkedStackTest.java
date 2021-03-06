c86ffd042e8bd28885720a4ee5f64c6562e7a67d
Amar Gandhi <amargandhi10@gmail.com>
2021-03-06 04:00:03 -0800

    Make test for TrailingWhitespace

--- LinkedStackTest.java
+++ LinkedStackTest.java
@@ -6,6 +6,8 @@ public class LinkedStackTest {
     static final String GIVEN_INFIX_EXPRESSION = "a * b / (c - a) + d * e";
     static final String EXPECTED_POSTFIX = "ab*ca-/de*+";
     
+    static final String INFIX_TRAILING = GIVEN_INFIX_EXPRESSION + "\t\r\n  \t\r\n     \t\r\n";
+    
     public static void main(String[] args) {
     
     }
@@ -13,5 +15,11 @@ public class LinkedStackTest {
     @Test
     public void convertToPostfix_Given() {
         assertEquals(EXPECTED_POSTFIX, Converter.convertToPostfix(GIVEN_INFIX_EXPRESSION.toCharArray()));
+    
+    @Test
+    public void convertToPostfix_TrailingWhitespace() {
+        assertEquals(EXPECTED_GIVEN, Converter.convertToPostfix(INFIX_TRAILING.toCharArray()));
+    }
+    
     }
 }
