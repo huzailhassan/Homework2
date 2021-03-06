f313f4fedbadb412a3e63fe929543df8ffb64eea
Amar Gandhi <amargandhi10@gmail.com>
2021-03-05 23:21:34 -0800

    Fix converterToPostfix to use old book-like code

--- Convertor.java
+++ Convertor.java
@@ -15,41 +15,32 @@ public class Convertor {
     }
     
     public static String convertToPostfix(char[] infix) {
-    
+        
         LinkedStack<Character> operatorStack = new LinkedStack<Character>();
         String postFix = "";
         // These will be char because we are reading in a string
-        char nextCharacter;
         char topOperator;
-    
-        int i = 0;
         
-        while (infix[i] <= infix.length) {
-            nextCharacter = infix[i];
+        for (char nextCharacter : infix) {
+            if (Character.isLetter(nextCharacter)) {
+                postFix += nextCharacter;
+                continue;   // not strictly necessary
+            }
             switch (nextCharacter) {
                 case '^':
                     operatorStack.push(nextCharacter);
-                    i++;
                     break;
                 case '+':
                 case '-':
-                    while (operatorStack.isEmpty() == false && 1 <= precedence(operatorStack.peek())) {
-                        operatorStack.pop();
-                    }
-                    operatorStack.push(nextCharacter);
-                    i++;
-                    break;
                 case '*':
                 case '/':
-                    while (operatorStack.isEmpty() == false && 2 <= precedence(operatorStack.peek())) {
-                        operatorStack.pop();
+                    while (!operatorStack.isEmpty() && precedence(nextCharacter) <= precedence(operatorStack.peek())) {
+                        postFix += operatorStack.pop();
                     }
                     operatorStack.push(nextCharacter);
-                    i++;
                     break;
                 case '(':
                     operatorStack.push(nextCharacter);
-                    i++;
                     break;
                 case ')':
                     topOperator = operatorStack.pop();
@@ -57,21 +48,18 @@ public class Convertor {
                         postFix += topOperator;
                         topOperator = operatorStack.pop();
                     }
-                    i++;
                     break;
                 //For any other characters. 
                 default:
-                    operatorStack.push(nextCharacter);
-                    i++;
                     break;
             }
-            while (!operatorStack.isEmpty()) {
-                topOperator = (char) operatorStack.pop();
-                postFix += topOperator;
-            }
-            return postFix;
         }
         
-        return null;
+        while (!operatorStack.isEmpty()) {
+            topOperator = operatorStack.pop();
+            postFix += topOperator;
+        }
+        
+        return postFix;
     }
 }
